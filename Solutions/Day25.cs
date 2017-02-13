f3cb26600e1ae0382b84c0ce1db114980d2caadf
Ola Thunberg <ola.thunberg@gmail.com>
2017-02-13 22:10:44 +0100

    Day25

---
+++ Solutions/Day25.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Solutions
+{
+    public class Day25
+    {
+        public static void Solve()
+        {
+            var result1 = GetCode(3010, 3019);
+            Console.WriteLine($"Day 25: Result part 1: {result1:N0}");   // 8997277
+
+            Console.WriteLine();
+        }
+
+        public static long GetCode(uint row, uint col)
+        {
+            var codeNum = GetCodeNumber(row, col);
+
+            var code = 20151125L;
+            for (int i = 1; i < codeNum; i++)
+                code = code * 252533 % 33554393;
+
+            return code;
+        }
+
+        public static long GetCodeNumber(uint row, uint col)
+        {
+            var code = 1L;
+
+            for (int i = 1; i < row; i++)
+                code += i;
+            for (int i = 2; i <= col; i++)
+                code += (i+row-1);
+
+            return code;
+        }
+    }
+}
---
+++ Tests/Day25Tests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Solutions.Day25;
+
+namespace Tests
+{
+    [TestClass]
+    public class Day25Tests
+    {
+        [TestMethod]
+        public void Day25Part1()
+        {
+            Assert.AreEqual(01, GetCodeNumber(1,1));
+            Assert.AreEqual(02, GetCodeNumber(2,1));
+            Assert.AreEqual(03, GetCodeNumber(1,2));
+            Assert.AreEqual(04, GetCodeNumber(3,1));
+            Assert.AreEqual(05, GetCodeNumber(2,2));
+            Assert.AreEqual(06, GetCodeNumber(1,3));
+            Assert.AreEqual(07, GetCodeNumber(4,1));
+            Assert.AreEqual(08, GetCodeNumber(3,2));
+            Assert.AreEqual(09, GetCodeNumber(2,3));
+            Assert.AreEqual(10, GetCodeNumber(1,4));
+            Assert.AreEqual(11, GetCodeNumber(5,1));
+            Assert.AreEqual(12, GetCodeNumber(4,2));
+            Assert.AreEqual(13, GetCodeNumber(3,3));
+            Assert.AreEqual(14, GetCodeNumber(2,4));
+            Assert.AreEqual(15, GetCodeNumber(1,5));
+            Assert.AreEqual(16, GetCodeNumber(6,1));
+            Assert.AreEqual(17, GetCodeNumber(5,2));
+            Assert.AreEqual(18, GetCodeNumber(4,3));
+            Assert.AreEqual(19, GetCodeNumber(3,4));
+            Assert.AreEqual(20, GetCodeNumber(2,5));
+            Assert.AreEqual(21, GetCodeNumber(1,6));
+
+            Assert.AreEqual(10600672, GetCode(4, 5));
+            Assert.AreEqual(33511524, GetCode(1, 6));
+            Assert.AreEqual(27995004, GetCode(6, 6));
+            Assert.AreEqual(31916031, GetCode(2, 1));
+            Assert.AreEqual(6796745, GetCode(6, 2));
+        }
+    }
+}
